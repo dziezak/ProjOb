462967ecb00f8675aefa27d17657fad21d00e256
dziezak <01189082@pw.edu.pl>
2025-04-17 20:19:55 +0200

    Nie działa jeszce Tworzenie ataku, czyli najwazniejsze jeszce nie dziala

---
+++ Rouge/ActionHandler/Handlers/ChooseEnemyHandler.cs
@@ -0,0 +1,33 @@
+namespace Rouge.ActionHandler;
+
+public class ChooseEnemyHandler : ActionHandlerBase
+{
+    public override void Handle(char input, Room room, Player player)
+    {
+        if (player.IsSelectingEnemies && char.IsDigit(input))
+        {
+            var enemiesNearBy = room.GetEnemiesNearBy(player.Y, player.X);
+            int enemyIndex = int.Parse(input.ToString());
+
+            if (enemyIndex >= 0 & enemyIndex < enemiesNearBy.Count)
+            {
+                player.lastCharacter = input;
+                var selectedEnemy = enemiesNearBy[enemyIndex];
+                GameDisplay.Instance?.AddLogMessage($"Witcher decided to fight {selectedEnemy.GetName()}");
+                player.SelectedEnemy = selectedEnemy;
+                player.IsSelectingEnemies = false;
+                player.Fight(); 
+            }
+            else
+            {
+                GameDisplay.Instance?.AddLogMessage("Invalid enemy selection");
+            }
+            UpdateUI(room, player);
+        }
+        else
+        {
+            base.Handle(input, room, player);
+        }
+    }
+    
+}
